6575f874db91424f751012c21e5d352b2d71c062
Diego Roberto <diego-roberto@hotmail.com>
2023-03-11 10:42:05 -0300

    remoção de arquivos desnecessários

--- frontend/src/app/views/constructions/form/construction-form/components/construction-generic/can-merge-construction.ts
+++
@@ -1,6 +0,0 @@
-import { CanMerge } from 'app/shared/util/generic/form/can-merge';
-import { Construction } from 'app/shared/models/construction.model';
-
-export interface CanMergeConstruction extends CanMerge<Construction> {
-    merge(model: Construction);
-}
